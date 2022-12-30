21795cb0809d42309e68f6752418020185669cf3
techboydk <003yadavdipesh@gmail.com>
2022-12-30 23:14:22 +0530

    wip

--- script.js
+++ script.js
@@ -43,7 +43,15 @@ const possibleComb = (str) =>{
 const form = document.getElementById('form'),
 input = document.getElementById('email'),
 displayGmails = document.querySelector('.generated-mail'),
+next = document.querySelector('.next-btn'),
+prev = document.querySelector('.prev-btn'),
 gmailContainer = document.querySelector('.all-gmail');
+
+let initial = 0,
+final = 10,
+n=0,
+allGmail=[];
+
 
 const createGmailDiv = (gmail,domain) =>{
   const div = document.createElement('div');
@@ -58,15 +66,52 @@ const createGmailDiv = (gmail,domain) =>{
 }
 
 
+
+
 form.addEventListener("submit", (e)=>{
   e.preventDefault()
   const inputValue = input.value.split('@')
   Gmail = inputValue[0]
   Domain = inputValue[1]
-  const allGmail = possibleComb(Gmail)
-  displayGmails.classList.add('active')
-  allGmail.map(gmail=>{
-    gmailContainer.appendChild(createGmailDiv(gmail,Domain));
-  })
+  allGmail=[]
+  n=0;
+  allGmail = possibleComb(Gmail)
+  n = allGmail.length;
+
+  //empaty container
   
+  displayGmails.classList.add('active')
+
+  final = n<final?n:final;
+  gmailContainer.innerHTML =''
+  DisplayGmails(initial,final);
+
+})
+
+const Next = (i,f,n) =>{
+  i = i>=0 && n<f? 0 : f;
+  f = n<f ? n : (n-f)>10? f+10 : n;
+  
+  gmailContainer.innerHTML =''
+  DisplayGmails(i,f);
+  console.log(i,f,n)
+}
+const Prev = (i,f,n) =>{
+
+}
+
+const DisplayGmails = (i,f) =>{
+for (let j = i; j < f; i++) {
+  let gmail = allGmail[j];
+  gmailContainer.appendChild(createGmailDiv(gmail,Domain));
+}
+}
+
+next.addEventListener("click",()=>{
+  Next(initial,final,n);
+})
+prev.addEventListener("click",()=>{
+  Prev(initial,final,n);
+  gmailContainer.innerHTML =''
+  DisplayGmails(allGmail);
 })
