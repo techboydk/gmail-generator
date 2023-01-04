8b83ddc4c7b72b16eee6450821de3aa74c6d6cd0
techboydk <003yadavdipesh@gmail.com>
2023-01-04 21:45:53 +0530

    wip

--- script.js
+++ script.js
@@ -1,135 +1,156 @@
-const addDot=(char)=>{
+const addDot = (char) => {
   let arr = []
   arr.push(char)
-  arr.push(char+'.')
-  arr.push('.'+char)
-  arr.push('.'+char+'.')
+  arr.push(char + '.')
+  arr.push('.' + char)
+  arr.push('.' + char + '.')
   return arr
 }
-const possibleComb = (str) =>{
+const possibleComb = (str) => {
   let len = str.length
-  
-  if(len%2){
-    let n = (len-1)/2
+
+  if (len % 2) {
+    let n = (len - 1) / 2
     let middleChar = [str[n]]
-    for(let i = 0; i<n;i++){
+    for (let i = 0; i < n; i++) {
       let temp = []
-      middleChar.map((char)=>{
-        addDot(char).map(c=>temp.push(c))
+      middleChar.map((char) => {
+        addDot(char).map(c => temp.push(c))
       })
-      temp.map((c,j) =>{
-        middleChar[j] = str[n-i-1]+c+str[n+i+1]
+      temp.map((c, j) => {
+        middleChar[j] = str[n - i - 1] + c + str[n + i + 1]
       })
     }
     return middleChar;
-  }else{
-    let n = (len-2)/2
-    let middleChar = [str[(len/2)-1]+str[len/2],str[(len/2)-1]+'.'+str[len/2]]
-    for(let i = 0; i<n;i++){
+  } else {
+    let n = (len - 2) / 2
+    let middleChar = [str[(len / 2) - 1] + str[len / 2], str[(len / 2) - 1] + '.' + str[len / 2]]
+    for (let i = 0; i < n; i++) {
       let temp = []
-      middleChar.map((char)=>{
-        addDot(char).map(c=>temp.push(c))
+      middleChar.map((char) => {
+        addDot(char).map(c => temp.push(c))
       })
-      temp.map((c,j) =>{
-        middleChar[j] = str[n-i-1]+c+str[n+i+2]
+      temp.map((c, j) => {
+        middleChar[j] = str[n - i - 1] + c + str[n + i + 2]
       })
     }
 
     return middleChar
   }
 }
-
-//variables
-const generateBtn = document.getElementById('generate'),
-input = document.getElementById('email'),
-displayGmails = document.querySelector('.generated-mail'),
-next = document.querySelector('.next-btn'),
-prev = document.querySelector('.prev-btn'),
-copyBtn = document.querySelector('.copy-btn'),
-currentGmailCount = document.querySelector('.current'),
-totalGmailCount = document.querySelector('.total'),
-gmailContainer = document.querySelector('.all-gmail');
-
-let n=0,
-current =0,
-allGmail;
-
-const createGmailDiv = (gmail,domain) =>{
+//gmail container template
+const createGmailContainer = (username) => {
   const div = document.createElement('div');
-
-  div.className = 'gmail'
-  text = `${gmail}@${domain}`
-  div.innerHTML = text
+  div.className = 'gmail';
+  const gmail = `${username}@${domain}`
+  div.innerHTML = gmail;
   return div;
 }
+const createListItem = (index) => {
+  const li = document.createElement('li'),
+    span = document.createElement('span');
+  span.className = 'item'
+  li.className = index ? '' : 'active';
+  span.innerHTML = index + 1;
+  li.appendChild(span);
+  return li;
+}
+
+//constant
+const generateBtn = document.getElementById('generate'),
+  select = document.getElementById('gmailperpage'),
+  input = document.getElementById('username'),
+  gmailContainer = document.querySelector('.gmail-container'),
+  totalCount = document.querySelector('.total span'),
+  currentCount = document.querySelector('.current'),
+  paginationContainer = document.querySelector('.pagination ul');
+
+//variables
+let allUserName = [],
+  inputValue,
+  selectValue,
+  domain,
+  pageNum = 1,
+  numOfPage,
+  username,
+  pagination = document.querySelectorAll('.pagination li');
+
+///onchange get value from input and select
+const onChange = () => {
+  pageNum = '1'
+  inputValue = input.value.split('@');
+  selectValue = Number(select.value);
+  username = inputValue[0];
+  domain = inputValue[1];
+  currentCount.firstElementChild.innerHTML= (pageNum-1)*selectValue+1;
+  currentCount.lastElementChild.innerHTML= pageNum*selectValue;
+  numOfPage = Math.ceil(allUserName.length / selectValue);
+  gmailContainer.innerHTML = ''
+  allUserName.map((username, index) => {
+    if (index < selectValue * pageNum && index > (pageNum - 1) * selectValue - 1) {
+      gmailContainer.appendChild(createGmailContainer(username));
+    }
+  })
+  paginationContainer.innerHTML = ''
+  if (numOfPage > 1) {
+    for (let i = 0; i < numOfPage; i++) {
+      paginationContainer.appendChild(createListItem(i));
+    }
+    pagination = document.querySelectorAll('.pagination li');
+  }
+}
+input.onchange = onChange;
+select.onchange = onChange;
 
 
-
-
-generateBtn.addEventListener("click", (e)=>{
+//prevent to submit and show generated gmail
+generateBtn.addEventListener('click', e => {
   e.preventDefault()
-  const inputValue = input.value.split('@')
-  Gmail = inputValue[0]
-  Domain = inputValue[1]
-  allGmail=[];
-  n=0;
-  current = 0;
-  
-  if(input.value.split('').includes('@') && Domain && Gmail){
-    allGmail = possibleComb(Gmail)
-    n = allGmail.length;
-    displayGmails.classList.add('active')
-    input.style.borderColor = 'black'
-  }else{
-   input.focus() 
-   input.style.borderColor = 'red'
-  }
-
-  if(n>1){
-    allGmail.shift()
-    n = allGmail.length;
-  }
-  
-  totalGmailCount.innerHTML = n;
-  currentGmailCount.innerHTML = current+1;
+  onChange()
   gmailContainer.innerHTML = ''
-  gmailContainer.appendChild(createGmailDiv(allGmail[current],Domain));
-  input.value = ' ';
+  allUserName = possibleComb(username);
+  numOfPage = Math.ceil(allUserName.length / selectValue);
+  totalCount.innerHTML = allUserName.length;
+  allUserName.map((username, index) => {
+    if (index < selectValue * pageNum && index > (pageNum - 1) * selectValue - 1) {
+      gmailContainer.appendChild(createGmailContainer(username));
+    }
+  })
+
+  //pagination
+  if (numOfPage > 1) {
+    paginationContainer.innerHTML = ''
+    for (let i = 0; i < numOfPage; i++) {
+      paginationContainer.appendChild(createListItem(i));
+    }
+    pagination = document.querySelectorAll('.pagination li');
+  }
 })
 
-next.addEventListener("click", ()=>{
-  if(current+1<n){
-    current +=1;
-  }else{
-    current =0;
-  }
-  gmail = allGmail[current]
-  gmailContainer.innerHTML = ''
-  gmailContainer.appendChild(createGmailDiv(gmail,Domain));
-  currentGmailCount.innerHTML = current+1;
-});
-prev.addEventListener("click", ()=>{
-  if(current>0){
-    current -=1;
-  }else{
-    current =n-1;
-  }
-  gmail = allGmail[current]
-  gmailContainer.innerHTML = ''
-  gmailContainer.appendChild(createGmailDiv(gmail,Domain));
-  currentGmailCount.innerHTML = current+1;
-  
-});
 
-//copy text 
-document.addEventListener("click", (e)=>{
+// all click handler
+
+document.addEventListener('click', e => {
   e.preventDefault();
-  if(e.target.classList.contains('fa-regular')){
-    console.log('find')
-    let text = `${allGmail[current]}@${Domain}`
-  navigator.clipboard.writeText(text);
-  copyBtn.classList.add('copied');
-  }else{
-    copyBtn.classList.remove('copied');
+  if (e.target.classList.contains('item')) {
+    pageNum = Number(e.target.innerHTML);
+    gmailContainer.innerHTML = ''
+    allUserName.map((username, index) => {
+      if (index < selectValue * pageNum && index > (pageNum - 1) * selectValue -1) {
+        gmailContainer.appendChild(createGmailContainer(username));
+      }
+    })
+    currentCount.firstElementChild.innerHTML= (pageNum-1)*selectValue+1;
+    currentCount.lastElementChild.innerHTML= pageNum*selectValue;
+    pagination.forEach(el => {
+      if (el.classList.contains('active')) {
+        el.classList.remove('active')
+      }
+    })
+    e.target.parentElement.classList.add('active')
   }
+
 })
+
+
+
